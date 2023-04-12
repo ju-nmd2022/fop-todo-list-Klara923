71693f73866fe0586b147a4f5bfa8a2243fbecb4
Klara923 <117095791+Klara923@users.noreply.github.com>
2023-04-12 10:45:11 +0200

    finished to do list

--- todo.js
+++ todo.js
@@ -2,43 +2,74 @@ const inputElement = document.getElementById("inputElement");
 const toDoItemsListElement = document.getElementById("toDoItemsList");
 const addButtonElement = document.getElementById("addButton");
 
+// Load the to-do list from local storage if it exists
+let toDoList = JSON.parse(localStorage.getItem("toDoList")) || [];
+
+// arrow function with (item) was made with help of chatGPT
+toDoList.forEach((item) => {
+  const toDoItem = createToDoItem(item.text, item.checked);
+  toDoItemsListElement.appendChild(toDoItem);
+});
+
+// Adding toDoItem to the list by clicking on the addButtonElement
 addButtonElement.addEventListener("click", add);
 
+// Add function, if there is nothing in the input element there is alert on the screen
+// If there is a text in the value toDoItem has text from input and check icon which is not checked
 function add() {
   if (inputElement.value.trim() === "") {
     alert("Add something to the list!");
   } else {
-    const toDoItem = document.createElement("li");
-    toDoItem.setAttribute("class", "toDoItem");
-    toDoItem.innerHTML = inputElement.value;
-
-    const checkIconElement = document.createElement("i");
-    checkIconElement.classList.add("far", "fa-circle-check");
-    toDoItem.appendChild(checkIconElement);
-    checkIconElement.addEventListener("click", checkedToDoItem);
-
-    const thrashIconElement = document.createElement("i");
-    thrashIconElement.classList.add("fas", "fa-trash");
-    thrashIconElement.addEventListener("click", removeToDoItem);
-    toDoItem.appendChild(thrashIconElement);
-
+    const toDoItem = createToDoItem(inputElement.value, false);
+    toDoList.push({ text: inputElement.value, checked: false });
     toDoItemsListElement.appendChild(toDoItem);
     inputElement.value = "";
+    saveToDoList();
   }
 }
 
-// This solution was creatted with help from chatGPT
+// CreateToDoItem function creates toDoItem element which has text, check icon and thrash icon inside
+function createToDoItem(text, checked) {
+  const toDoItem = document.createElement("li");
+  toDoItem.setAttribute("class", "toDoItem");
+  toDoItem.innerHTML = text;
 
-function removeToDoItem(event) {
-  let toDoItem = event.target.parentNode;
-  toDoItem.remove();
-  console.log("balablablab");
+  const checkIconElement = document.createElement("i");
+  checkIconElement.classList.add("far", "fa-circle-check");
+  if (checked) {
+    toDoItem.classList.add("checked");
+    checkIconElement.classList.replace("far", "fas");
+  }
+  checkIconElement.addEventListener("click", () => {
+    toDoItem.classList.toggle("checked");
+    checkIconElement.classList.toggle("far");
+    checkIconElement.classList.toggle("fas");
+    // The following two lines of text were made with help from chatGPT
+    const index = Array.from(toDoItemsListElement.children).indexOf(toDoItem);
+    toDoList[index].checked = toDoItem.classList.contains("checked");
+    saveToDoList();
+  });
+
+  const thrashIconElement = document.createElement("i");
+  thrashIconElement.classList.add("fas", "fa-trash");
+  thrashIconElement.addEventListener("click", () => {
+    toDoItem.remove();
+    // The following line of text were made with help from chatGPT
+    const index = Array.from(toDoItemsListElement.children).indexOf(toDoItem);
+    toDoList.splice(index, 1);
+    saveToDoList();
+  });
+
+  const iconContainer = document.createElement("div");
+  iconContainer.setAttribute("class", "iconContainer");
+  iconContainer.appendChild(checkIconElement);
+  iconContainer.appendChild(thrashIconElement);
+  toDoItem.appendChild(iconContainer);
+
+  return toDoItem;
 }
-// The toggle() method toggles between hide() and show()
-function checkedToDoItem(event) {
-  let toDoItem = event.target.parentNode;
-  toDoItem.classList.toggle("checked");
-  let checkIconElement = toDoItem.querySelector(".fa-circle-check");
-  checkIconElement.classList.toggle("far");
-  checkIconElement.classList.toggle("fas");
+
+// toDoList with new toDoItems inside is saving in the localStorage
+function saveToDoList() {
+  localStorage.setItem("toDoList", JSON.stringify(toDoList));
 }
